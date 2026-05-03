4d7d341f8d0eeacb47ce0da7e1a941d2e4652466
denrouz <rouzden@inbox.ru>
2026-05-03 20:21:26 +0300

    Добавлено преобразование коммитов в датафрейм

---
+++ R/etl_commits.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+# Преобразование данных GitHub API в аккуратный датафрейм
+transform_commits <- function(commits, repo_name) {
+  df <- commits %>%
+    transmute(
+      repo = repo_name,
+      author = commit.author.name,
+      date = as.POSIXct(commit.author.date),
+      message = commit.message,
+      additions = NA_integer_,
+      deletions = NA_integer_,
+      files_changed = NA_integer_
+    )
+
+  return(df)
+}
