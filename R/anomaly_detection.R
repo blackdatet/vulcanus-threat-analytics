067585dc121581a20cafbece3b086f6f565395e0
IKhlestkov <ivankhlestkov@mail.ru>
2026-05-09 18:11:34 +0300

    Улучшено обнаружение аномалий

--- R/anomaly_detection.R
+++ R/anomaly_detection.R
@@ -1,16 +1,47 @@
-library(dplyr)
+# Модуль поиска аномалий
+# Автор: IKhlestkov
 
-# коммиты ночью
+library(dplyr)
+library(lubridate)
+
+# Поиск ночных коммитов
 detect_night_commits <- function(df) {
+
   df %>%
-    mutate(hour = as.numeric(format(date, "%H"))) %>%
+    mutate(hour = hour(date)) %>%
     filter(hour >= 0 & hour <= 5)
 }
 
-# слишком частые коммиты (условно)
+# Поиск авторов с аномально высокой активностью
 detect_high_activity <- function(df) {
-  df %>%
+
+  stats <- df %>%
     group_by(author) %>%
-    summarise(commits = n()) %>%
-    filter(commits > mean(commits) * 2)
+    summarise(commits = n(), .groups = "drop")
+
+  avg_commits <- mean(stats$commits)
+  sd_commits <- sd(stats$commits)
+
+  stats %>%
+    mutate(
+      z_score = (commits - avg_commits) / sd_commits
+    ) %>%
+    filter(z_score > 1)
+}
+
+# Общий список подозрительных авторов
+detect_suspicious_authors <- function(df) {
+
+  high_activity <- detect_high_activity(df)
+
+  night_commits <- detect_night_commits(df) %>%
+    distinct(author)
+
+  suspicious <- full_join(
+    high_activity,
+    night_commits,
+    by = "author"
+  )
+
+  return(suspicious)
 }
