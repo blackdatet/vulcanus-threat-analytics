49b6934bda5f935fbf95e7fa1fbd7009a5ea7b6d
IKhlestkov <ivankhlestkov@mail.ru>
2026-05-09 18:01:30 +0300

    Расширены метрики анализа коммитов

--- R/metrics.R
+++ R/metrics.R
@@ -1,21 +1,51 @@
-library(dplyr)
+# Модуль расчета метрик по Git-коммитам
+# Автор: IKhlestkov
 
-# количество коммитов на автора
+library(dplyr)
+library(lubridate)
+
+# Количество коммитов на автора
 get_commits_per_author <- function(df) {
   df %>%
     group_by(author) %>%
-    summarise(commits = n())
+    summarise(commits = n(), .groups = "drop") %>%
+    arrange(desc(commits))
 }
 
-# общее количество коммитов
+# Общее количество коммитов
 get_total_commits <- function(df) {
   nrow(df)
 }
 
-# активность по часам
+# Активность по часам
 get_activity_by_hour <- function(df) {
   df %>%
-    mutate(hour = as.integer(format(date, "%H"))) %>%
+    mutate(hour = hour(date)) %>%
     group_by(hour) %>%
-    summarise(commits = n())
+    summarise(commits = n(), .groups = "drop") %>%
+    arrange(hour)
+}
+
+# Топ авторов по количеству коммитов
+get_top_authors <- function(df, n_authors = 5) {
+  get_commits_per_author(df) %>%
+    slice_head(n = n_authors)
+}
+
+# Среднее количество коммитов в день
+get_average_commits_per_day <- function(df) {
+  df %>%
+    mutate(day = as.Date(date)) %>%
+    group_by(day) %>%
+    summarise(commits = n(), .groups = "drop") %>%
+    summarise(avg_commits_per_day = mean(commits))
+}
+
+# Распределение коммитов по дням недели
+get_commits_by_weekday <- function(df) {
+  df %>%
+    mutate(weekday = weekdays(as.Date(date))) %>%
+    group_by(weekday) %>%
+    summarise(commits = n(), .groups = "drop") %>%
+    arrange(desc(commits))
 }
