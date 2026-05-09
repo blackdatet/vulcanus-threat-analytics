e663a2b98204bd8739f85df1e75f1f8df1ed07bc
denrouz <rouzden@inbox.ru>
2026-05-09 17:54:17 +0300

    Обновлен скрипт загрузки данных через get_clean_commits

--- data-raw/load_commits.R
+++ data-raw/load_commits.R
@@ -1,16 +1,10 @@
 source("R/github_api.R")
 source("R/etl_commits.R")
 
-# Пример загрузки данных из публичного GitHub-репозитория
-commits_raw <- get_github_commits(
+# Загрузка примерных данных из публичного GitHub-репозитория
+example_commits <- get_clean_commits(
   owner = "torvalds",
-  repo = "linux",
-  per_page = 30
-)
-
-example_commits <- transform_commits(
-  commits = commits_raw,
-  repo_name = "torvalds/linux"
+  repo = "linux"
 )
 
 save(example_commits, file = "data/example_commits.rda")
